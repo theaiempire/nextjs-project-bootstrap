d19c69a48796c076f2e7fbb7ec4627a125da0b71
theaiempire <bharatgoyenka@gmail.com>
2025-07-06 19:18:08 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751809687301

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_761b26fd._.js",
+  "static/chunks/src_0d33601d._.js"
+],
+    source: "dynamic"
+});
